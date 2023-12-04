a2cf4fcfe5589b197632dec70b714bf7a647d6ee
choi <nipper@naver.com>
2023-12-04 15:33:12 +0900

    test file delete

--- src/main/java/com/example/board/mapper/TestMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.board.mapper;
-
-import com.example.board.model.Test;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface TestMapper {
-    Test test() throws Exception;
-}
